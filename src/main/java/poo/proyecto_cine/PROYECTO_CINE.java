06e2e8ad86fc129730117a5cb40c5b375ff5662f
Gerson Molina <moli8717@gmail.com>
2026-04-16 22:37:17 -0500

    estructura inicial del proyecto en NetBeans

---
+++ src/main/java/poo/proyecto_cine/PROYECTO_CINE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.proyecto_cine;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class PROYECTO_CINE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
